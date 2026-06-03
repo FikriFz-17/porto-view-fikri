dc51912184a7bfa740da6f3c6e8a177ce3c19f51
yoru <fikri36987@gmail.com>
2026-06-03 11:44:26 +0700

    fix: remove vite-plugin-prerender

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,9 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import prerender from "vite-plugin-prerender";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    prerender({
-      staticDir: path.join(__dirname, "dist"),
-      routes: ["/", "/projects"],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
